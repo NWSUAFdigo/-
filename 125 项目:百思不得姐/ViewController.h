46ba7acff7c6e5d739ec016f7400e80c46a9c11a
NWSUAF <wudi9426@163.com>
2016-06-23 10:17:38 +0800

    创建了百思不得姐项目

---
+++ 125 项目:百思不得姐/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  125 项目:百思不得姐
+//
+//  Created by wudi on 16/6/23.
+//  Copyright © 2016年 wudi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 125 项目:百思不得姐/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  125 项目:百思不得姐
+//
+//  Created by wudi on 16/6/23.
+//  Copyright © 2016年 wudi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
